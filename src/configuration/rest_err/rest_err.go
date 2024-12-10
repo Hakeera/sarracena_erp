826125347b090a594323c415bfd4a5ecc0c6dbdb
Hakeera <xhcbhudgfv@gmail.com>
2024-12-10 20:28:01 -0300

    Padronizando erros

--- src/configuration/rest_err/rest_err.go
+++ src/configuration/rest_err/rest_err.go
@@ -2,14 +2,18 @@ package rest_err
 
 type RestErr struct {
 	Message string `json:"message"`
-	Err string 
-	Code int
-	Causes []Causes
+	Err string `json: "error"`
+	Code int `json: "cpde"`
+	Causes []Causes `json: "causes"`
 }
 
 type Causes struct {
-	Field string
-	Message string
+	Field string `json: "field"`
+	Message string `json: "message"`
+}
+
+func (r *RestErr) Error() sting {
+	return r.Message
 }
 
 func NewRestError(message, err string, code int, causes []Causes) *RestErr {
@@ -27,5 +31,38 @@ func NewBadRequestError(message, err string, code int, causes []Causes) *RestErr
 		Message: message,
 		Err: "bad request",
 		Code: http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: "bad request",
+		Code: http.StatusBadRequest,
+		Causes: causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: "bad request",
+		Code: http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: "bad request",
+		Code: http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err: "bad request",
+		Code: http.StatusForbidden,
 	}
 }
