bf312caf465f8528d4024accf715fb5a4000f12a
Hakeera <jxhcbhudgfv@gmail.com>
2025-01-08 19:53:19 -0300

    Ajustes na  documentação

--- main.go
+++ main.go
@@ -11,33 +11,33 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+// main é o ponto de entrada da aplicação. Ele inicializa variáveis de ambiente, 
+// configura a conexão com o banco de dados, instância serviços e controladores,
+// configura rotas HTTP e inicia o servidor na porta 8080.
 func main() {
-    logger.Info("About to start user application")
+	logger.Info("About to start user application")
     
 	// Carregar Variáveis de Ambiente
-    err:= godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
     
 	// Conexão com Banco de Dados
 	database.InitDatabase()
 	defer database.CloseDatabase()
 
-	// Inicilizar Dependências
+	// Inicializar Dependências
 	service := service.NewUserDomainService()
 	userController := controller.NewUserControllerInterface(service)
 
 	// Configurar Roteador
-    router := gin.Default()
-    routes.InitRoutes(&router.RouterGroup, userController)
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup, userController)
 
-	// Iniciar servidor
-    if err:= router.Run(":8080"); err != nil {
-        log.Fatal(err)
-    }
-
-	
+	// Iniciar Servidor
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
-
-
--- src/view/convert_domain_to_response.go
+++ src/view/convert_domain_to_response.go
@@ -5,12 +5,15 @@ import (
 	"sarracena_erp/src/model/response"
 )
 
+// ConvertDomainToResponse transforma um domínio de usuário em uma estrutura JSON 
+// para ser enviada ao cliente.
+// Recebe um objeto que implementa a interface UserDomainInterface e retorna um UserResponse.
 func ConvertDomainToResponse(
 	userDomain model.UserDomainInterface,
- ) response.UserResponse {
+) response.UserResponse {
 	return response.UserResponse{
 		Email: userDomain.GetEmail(),
-		Name: userDomain.GetName(),
-		Age: userDomain.GetAge(),	
+		Name:  userDomain.GetName(),
+		Age:   userDomain.GetAge(),
 	}
 }
